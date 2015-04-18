c51d67f44dcc918a9dfbd1749aaca22668d5c67e
Jeff Swartz <swartz@tokbox.com>
2015-04-17 18:55:03 -0700

    Code for basics.step-4

--- LearningOpenTok/ViewController.m
+++ LearningOpenTok/ViewController.m
@@ -9,10 +9,15 @@
 #import <OpenTok/OpenTok.h>
 
 @interface ViewController ()
+<OTSessionDelegate>
 
 @end
 
 @implementation ViewController {
+    OTSession* _session;
+    NSString* _apiKey;
+    NSString* _sessionId;
+    NSString* _token;
 }
 
 #pragma mark - View lifecycle
@@ -20,6 +25,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self getSessionCredentials];
+}
+
+- (void)getSessionCredentials
+{
+    NSString* urlPath = SAMPLE_SERVER_BASE_URL;
+    urlPath = [urlPath stringByAppendingString:@"/session"];
+    NSURL *url = [NSURL URLWithString: urlPath];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:10];
+    [request setHTTPMethod: @"GET"];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
+        if (error){
+            NSLog(@"Error,%@, URL: %@", [error localizedDescription],urlPath);
+        }
+        else{
+            NSDictionary *roomInfo = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            _apiKey = [roomInfo objectForKey:@"apiKey"];
+            _token = [roomInfo objectForKey:@"token"];
+            _sessionId = [roomInfo objectForKey:@"sessionId"];
+            
+            if(!_apiKey || !_token || !_sessionId) {
+                NSLog(@"Error invalid response from server, URL: %@",urlPath);
+            } else {
+                [self doConnect];
+            }
+        }
+    }];
 }
 
 - (BOOL)prefersStatusBarHidden
@@ -38,6 +71,67 @@
     } else {
         return YES;
     }
+}
+#pragma mark - OpenTok methods
+
+- (void)doConnect
+{
+    // Initialize a new instance of OTSession and begin the connection process.
+    _session = [[OTSession alloc] initWithApiKey:_apiKey
+                                       sessionId:_sessionId
+                                        delegate:self];
+    OTError *error = nil;
+    [_session connectWithToken:_token error:&error];
+    if (error)
+    {
+        NSLog(@"Unable to connect to the session (%@)",
+              error.localizedDescription);
+    }
+}
+
+# pragma mark - OTSession delegate callbacks
+
+- (void)sessionDidConnect:(OTSession*)session
+{
+    NSLog(@"Connected to the session.");
+}
+
+- (void)sessionDidDisconnect:(OTSession*)session
+{
+    NSString* alertMessage =
+    [NSString stringWithFormat:@"Session disconnected: (%@)",
+     session.sessionId];
+    NSLog(@"sessionDidDisconnect (%@)", alertMessage);
+}
+
+- (void)session:(OTSession*)session
+streamCreated:(OTStream *)stream
+{
+    NSLog(@"session streamCreated (%@)", stream.streamId);
+}
+
+- (void)session:(OTSession*)session
+streamDestroyed:(OTStream *)stream
+{
+    NSLog(@"session streamDestroyed (%@)", stream.streamId);
+}
+
+- (void)  session:(OTSession *)session
+connectionCreated:(OTConnection *)connection
+{
+    NSLog(@"session connectionCreated (%@)", connection.connectionId);
+}
+
+- (void)    session:(OTSession *)session
+connectionDestroyed:(OTConnection *)connection
+{
+    NSLog(@"session connectionDestroyed (%@)", connection.connectionId);
+}
+
+- (void) session:(OTSession*)session
+didFailWithError:(OTError*)error
+{
+    NSLog(@"session didFailWithError: (%@)", error);
 }
 
 @end

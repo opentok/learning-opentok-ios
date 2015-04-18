152bb33a809810e1472f2e3e3ad2ce3c9b9851ff
Jeff Swartz <swartz@tokbox.com>
2015-04-17 18:23:24 -0700

    Code for basics.step-1 branch and README docs for all branches.

---
+++ LearningOpenTok/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.h
+//  Getting Started
+//
+//  Created by Jeff Swartz on 11/19/14.
+//  Copyright (c) 2014 TokBox, Inc. All rights reserved.
+
+#import "ViewController.h"
+#import <OpenTok/OpenTok.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController {
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:
+(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    if (UIUserInterfaceIdiomPhone == [[UIDevice currentDevice]
+                                      userInterfaceIdiom])
+    {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+@end
